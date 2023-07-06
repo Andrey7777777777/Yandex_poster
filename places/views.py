5ff4b2ad2c11922f92fa65857529ddcb3eff12ff
Andrey7777777777 <117460052+Andrey7777777777@users.noreply.github.com>
2023-07-06 18:44:40 +0300

    Upload GeoJSON

--- places/views.py
+++ places/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from places.models import Places
 
-# Create your views here.
+
+def index(request):
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": []}
+    places = Places.objects.all()
+    for place in places:
+        places_geojson["features"].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.latitude, place.longitude]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.place_id,
+                    "detailsUrl": "./static/places/moscow_legends.json"
+                }
+            })
+
+    data = {'places_geojson': places_geojson}
+    return render(request, "index.html", context=data)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from where_to_go import views
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
